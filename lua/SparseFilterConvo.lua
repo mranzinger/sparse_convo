752c8e253e480e9d5be96fc132e9381d1f830a56
Mike Ranzinger <mranzinger@alchemyapi.com>
2015-05-21 11:03:08 -0600

    Implementing the lua part of the module

--- lua/SparseFilterConvo.lua
+++ lua/SparseFilterConvo.lua
@@ -3,17 +3,53 @@ require 'cutorch'
 local SparseFilterConvo, parent = torch.class('nn.SparseFilterConvo',
                                               'nn.Module')
 
-function SparseFilterConvo:__init()
-    --TODO: Implement Me!
+function SparseFilterConvo:__init(nOutputPlanes,
+                                  kW, kH,
+                                  dkW, dkH)
+    --Assumes stride of 1 and sufficient padding to preserve input
+    --width and height
+    self.m_nOutputPlanes = nOutputPlanes
+    self.m_kW = kW
+    self.m_kH = kH
+    self.m_dkW = dkW or 2
+    self.m_dkH = dkH or 2
 end
 
-function SparseFilterConvo:reset(stdv)
-    --TODO: Implement Me!
+function SparseFilterConvo:reset()
+    
+    -- Input planes are the outer dimension so that we can stream in
+    -- planes and accumulate partial convolutions into the output buffer
+    local weightSizes = torch.LongStorage(self.m_nInputPlanes,
+                                          self.m_nOutputPlanes,
+                                          self.m_kH,
+                                          self.m_kW)
+
+    -- Use the Microsoft initialization method
+    self.weights = torch.randn(weightSizes):float()
+    self.bias = torch.zeros(self.m_nOutputPlanes):float()
+
+    local sdv = math.sqrt(2.0 / (self.m_nInputPlanes * self.m_kH * self.m_kW))
+
+    self.weights:mul(sdv)
+
+    -- If CUDA, then move the weights to the device
+    if self.m_isCuda then
+        self.weights = self.weights:cuda()
+        self.bias = self.bias:cuda()
+    end
+
 end
 
 function SparseFilterConvo:updateOutput(input)
     --TODO: Implement Me!
-    input.nn.SparseFilterConvo_updateOutput(self, input)
+    
+    if torch.type(input) == 'torch.FloatTensor' then 
+        input.nn.SparseFilterConvo_cpu_updateOutput(self, input)
+    elseif torch.type(input) == 'torch.CudaTensor' then
+        input.nn.SparseFilterConvo_cu_updateOutput(self, input)
+    else
+        error('Invalid tensor type: ' .. torch.type(input))
+    end
 
     return self.output
 end
@@ -30,5 +66,51 @@ function SparseFilterConvo:accGradParameters(input, gradOutput, scale)
     --TODO: Implement Me!
     input.nn.SparseFilterConvo_accGradParameters(self, input, gradOutput, scale)
 end
+
+function SparseFilterConvo::isCuda(ts)
+
+    if not ts then
+        error('Invalid nil tensor')
+    elseif torch.type(ts) == 'torch.CudaTensor' then
+        return true
+    elseif torch.type(ts) == 'torch.FloatTensor' then
+        return false
+    else
+        error('Unsupported tensor type: ' .. torch.type(ts))
+    end
+
+end
+
+function SparseFilterConvo::prepareSystem(ts)
+
+    local isCuda = self:isCuda(ts)
+
+    if self.m_isPrepared then
+        assert(self.m_isCuda == isCuda)
+        return
+    end
+
+    self.m_isCuda = isCuda
+
+    local chanIdx = 1
+
+    if ts:dim() == 4 then
+        chanIdx = 2
+    end
+
+    self.m_nInputPlanes = ts:size(chanIdx)
+
+    -- Initialize the weights
+    self:reset()
+
+end
+
+
+
+
+
+
+
+
 
 
--- src/SparseFilterConvolution.cpp
+++ src/SparseFilterConvolution.cpp
@@ -1,34 +1,34 @@
 
-#include "SparseFilterConvolution_cpp_impl.h"
+#include "SparseFilterConvolution_cpu_impl.h"
 
 using namespace sparse_convo;
 
-static int sparseconvo_SparseFilterConvo_cpp_updateOutput(lua_State *L)
+static int sparseconvo_SparseFilterConvo_cpu_updateOutput(lua_State *L)
 {
     return cpu::SparseFilterConvo::UpdateOutput(L);
 }
 
-static int sparseconvo_SparseFilterConvo_cpp_updateGradInput(lua_State *L)
+static int sparseconvo_SparseFilterConvo_cpu_updateGradInput(lua_State *L)
 {
     return cpu::SparseFilterConvo::UpdateGradInput(L);
 }
 
-static int sparseconvo_SparseFilterConvo_cpp_accGradParameters(lua_State *L)
+static int sparseconvo_SparseFilterConvo_cpu_accGradParameters(lua_State *L)
 {
     return cpu::SparseFilterConvo::AccGradParameters(L);
 }
 
 
-static const struct luaL_Reg sparseconvo_SparseFilterConvo_cpp__ [] = {
-    {"SparseFilterConvo_cpp_updateOutput", sparseconvo_SparseFilterConvo_cpp_updateOutput},
-    {"SparseFilterConvo_cpp_updateGradInput", sparseconvo_SparseFilterConvo_cpp_updateGradInput},
-    {"SparseFilterConvo_cpp_accGradParameters", sparseconvo_SparseFilterConvo_cpp_accGradParameters},
+static const struct luaL_Reg sparseconvo_SparseFilterConvo_cpu__ [] = {
+    {"SparseFilterConvo_cpu_updateOutput", sparseconvo_SparseFilterConvo_cpu_updateOutput},
+    {"SparseFilterConvo_cpu_updateGradInput", sparseconvo_SparseFilterConvo_cpu_updateGradInput},
+    {"SparseFilterConvo_cpu_accGradParameters", sparseconvo_SparseFilterConvo_cpu_accGradParameters},
     {NULL, NULL}  
 }; 
 
-static void sc_SparseFilterConvo_cpp_init(lua_State *L)
+static void sc_SparseFilterConvo_cpu_init(lua_State *L)
 {
     luaT_pushmetatable(L, "torch.FloatTensor");
-    luaT_registeratname(L, sparseconvo_SparseFilterConvo_cpp__, "nn");
+    luaT_registeratname(L, sparseconvo_SparseFilterConvo_cpu__, "nn");
     lua_pop(L,1);
 }
